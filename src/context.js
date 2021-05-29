c5d0bda431627e9f5c3925ad927f8eb75356d7d3
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-05-29 23:44:09 +0200

    completed on the localData

--- src/context.js
+++ src/context.js
@@ -9,13 +9,32 @@ export default class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading:true
+        loading:true,
+        type:'all',
+        capacity:1,
+        price:0,
+        minPrice:0,
+        maxPrice:0,
+        minSize:0,
+        maxSize:0,
+        breakfast:false,
+        pets:false
     }
 
     componentDidMount() {
-        let rooms = this.formatData(items)
+        let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room =>room.featured === true);
-        this.setState({rooms, featuredRooms, sortedRooms:rooms,loading:false});
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+        this.setState({
+            rooms,
+             featuredRooms,
+              sortedRooms:rooms,
+              loading:false,
+              price:maxPrice,
+              maxPrice,
+              maxSize
+            });
     }
 
     formatData(items){
@@ -34,12 +53,67 @@ export default class RoomProvider extends Component {
         let tempRooms = [...this.state.rooms];
         const room = tempRooms.find(room => room.id === id);
         return room;
+    };
+
+    handleChange = event =>{
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = event.target.name;
+       
+        this.setState(
+            {
+            [name]:value
+        },
+        this.filterRooms
+        )
+      };
+    filterRooms = () =>{
+        let {
+            rooms,
+            type,
+            capacity,
+            price,
+            minSize,
+            maxSize,
+            breakfast,
+            pets
+                      } = this.state  
+
+        let tempRooms = [...rooms];
+
+        capacity = parseInt(capacity)
+        price = parseInt(price)
+
+        if(type !== 'all') {
+             tempRooms = tempRooms.filter(room => room.type === type)
+        }   
+
+        if(capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        tempRooms= tempRooms.filter(room => room.price <= price);
+
+        tempRooms = tempRooms.filter(room =>room.size >= minSize && room.size <= maxSize);
+
+        if(breakfast){
+          tempRooms = tempRooms.filter(room => room.breakfast === true);
+        }
+
+        if(pets){
+          tempRooms = tempRooms.filter(room => room.pets === true);
+        }
+
+        this.setState({
+           sortedRooms: tempRooms 
+        })
+
     }
 
 
     render() {
         return (
-            <RoomContext.Provider value={{...this.state,getRoom:this.getRoom}}>
+            <RoomContext.Provider value={{...this.state,getRoom:this.getRoom,handleChange:this.handleChange}}>
                 {this.props.children}
             </RoomContext.Provider>
         )
@@ -47,5 +121,13 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props){
+        return <RoomConsumer>
+            {value => <Component {...props} context={value}/>}
+        </RoomConsumer>
+    }
+}
 
 export {RoomProvider, RoomConsumer,RoomContext}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,16 +9,16 @@ import FeaturedRooms from '../components/FeaturedRooms'
 export default function Home() {
     return (
         <>
-        <Hero>
-<Banner title='luxurious rooms' subtitle='deluxe room starting at 299$'>
-    <Link to = '/rooms' className='btn-primary'>
-         our rooms
-    </Link>
-</Banner>
-
-        </Hero>
+       <Hero>
+        <Banner title='luxurious rooms' subtitle='deluxe room starting at 299$'>
+         <Link to = '/rooms' className='btn-primary'>
+           our rooms
+         </Link>
+        </Banner>
+       </Hero>
         <Services/>
         <FeaturedRooms/>
+        
         </>
     )
 }
